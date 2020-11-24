737d14aaeb9f37804d84a563098124e72ed4e754
Scott <scottgdaniel@gmail.com>
2020-11-24 11:43:54 -0600

    abstracted shell scripts

--- code/parsers/parse_all.sh
+++ code/parsers/parse_all.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-# Passes all arguments through to parse_pacer.py, use -f for force and -d for debug (-fd for both)
-# Example: bash parse_all.sh -fd
+# After reading the directory path, passes all arguments through to parse_pacer.py; use -f for force and -d for debug (-fd for both)
+# Example: bash parse_all.sh ../../data/pacer -fd
 # See arguments documented in parse_pacer.py
-for courtdir in ../../data/pacer/*/; do 
+
+dir=$1
+shift
+for courtdir in $dir/*/; do 
     echo $courtdir;
     python parse_pacer.py $courtdir/html/ $courtdir/json/ "$@"; 
 done
--- code/parsers/parse_subset.sh
+++ code/parsers/parse_subset.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+dir=$1
+shift
 while getopts s:e: flag; do
     case "$flag" in
-        s)	
-			startdir=${OPTARG}
-			;;
+        s)  
+            startdir=${OPTARG}
+            ;;
         e)
-			enddir=${OPTARG}
-			;;
+            enddir=${OPTARG}
+            ;;
     esac
 done
 
 shift 4
-for courtdir in ../../data/pacer/*; do 
+for courtdir in $dir/*; do 
     if [ ! $(basename $courtdir) \< $(basename $startdir) ] && [ ! $(basename $courtdir) \> $(basename $enddir) ]
     then
-    	echo "Running on ${courtdir}"
-    	python parse_pacer.py $courtdir/html/ $courtdir/json/ "$@";
+        echo "Running on ${courtdir}"
+        python parse_pacer.py $courtdir/html/ $courtdir/json/ "$@";
     else
-    	echo "Skipping ${courtdir}";
+        echo "Skipping ${courtdir}";
     fi
 done
